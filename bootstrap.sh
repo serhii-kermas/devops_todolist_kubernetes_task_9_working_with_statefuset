9c4e424c7cc1e1652b780f4884ffb6dc47af9bdd
Serhii Kermas <serhii.kermas@gmail.com>
2025-09-01 15:57:44 +0300

    Solution-test

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+kubectl apply -f ./.infrastructure/namespace.yml
+kubectl apply -f ./.infrastructure/statefulSet-service.yml -n mysql
+kubectl apply -f ./.infrastructure/statefulSet-secret.yml -n mysql
+kubectl apply -f ./.infrastructure/statefulSet-configMap.yml -n mysql
+kubectl apply -f ./.infrastructure/statefulSet.yml -n mysql
+kubectl apply -f ./.infrastructure/pv.yml
+kubectl apply -f ./.infrastructure/pvc.yml
+kubectl apply -f ./.infrastructure/clusterIp.yml
+kubectl apply -f ./.infrastructure/nodeport.yml -n todoapp
+kubectl apply -f ./.infrastructure/secret.yml -n todoapp
+kubectl apply -f ./.infrastructure/configMap.yml
+kubectl apply -f ./.infrastructure/hpa.yml
+kubectl apply -f ./.infrastructure/deployment.yml -n todoapp
