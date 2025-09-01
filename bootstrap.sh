00abe5ad74be5b2bdf6857d80d7e210d641c6513
Serhii Kermas <serhii.kermas@gmail.com>
2025-09-01 16:47:28 +0300

    Solution-edits-1

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+kind create cluster --config cluster.yml
 kubectl apply -f ./.infrastructure/namespace.yml
-kubectl apply -f ./.infrastructure/statefulSet-service.yml -n mysql
-kubectl apply -f ./.infrastructure/statefulSet-secret.yml -n mysql
-kubectl apply -f ./.infrastructure/statefulSet-configMap.yml -n mysql
-kubectl apply -f ./.infrastructure/statefulSet.yml -n mysql
+kubectl apply -f ./.infrastructure/statefulSet-secret.yml 
+kubectl apply -f ./.infrastructure/statefulSet-service.yml 
+kubectl apply -f ./.infrastructure/statefulSet-configMap.yml 
+kubectl apply -f ./.infrastructure/statefulSet.yml
+kubectl apply -f ./.infrastructure/secret.yml 
+kubectl apply -f ./.infrastructure/clusterIp.yml 
+kubectl apply -f ./.infrastructure/nodeport.yml 
 kubectl apply -f ./.infrastructure/pv.yml
 kubectl apply -f ./.infrastructure/pvc.yml
-kubectl apply -f ./.infrastructure/clusterIp.yml
-kubectl apply -f ./.infrastructure/nodeport.yml -n todoapp
-kubectl apply -f ./.infrastructure/secret.yml -n todoapp
 kubectl apply -f ./.infrastructure/configMap.yml
 kubectl apply -f ./.infrastructure/hpa.yml
-kubectl apply -f ./.infrastructure/deployment.yml -n todoapp
+kubectl apply -f ./.infrastructure/deployment.yml 
